387199246da150bf4b5d821f452e06c3ff0d9166
Khan <mkhan@digitallynctech.om>
2018-07-17 17:31:45 +0530

    first message

---
+++ classTwentyThreeOnline.py
@@ -0,0 +1,61 @@
+git --> version control 
+--> repository management system
+
+
+100 devs-->server-->code(US) --> www.fb.com --> homepage
+
+100 devs -> single source --> to put all code 
+
+--> drive for code
+
+drive --> repository
+
+2 kinds
+-------
+local repository --> local machine
+remote repository --> server
+
+--> git 
+	--> technology 
+	--> install --> use it 
+	--> files / folders(directories)
+--> remote repository
+	--> git server (own)
+	--> third party services
+		->public repository --> free
+		->private repository --> paid
+			- github
+			- bitbucket
+			- gitlab
+
+--> download git and run (install)
+
+-->install git
+-->create a folder (gitDemoPy)
+-->initialise the local repository -- git init
+-->git status (status of the initialised folder)
+
+--> git architecture 
+--------------------
+
+local machine (folder) 
+	- working directory --> my folder/files
+	- staging area --> files will be hanging 
+	- local repository --> store the files
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
